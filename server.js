e183b6f87526cbd4dfd910c695650d25e1462670
Sashkata <alekspenev347@gmail.com>
2020-05-13 11:13:33 -0500

    More Server Changes to work when hosted

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
 const jsonServer = require("json-server");
 const server = jsonServer.create();
-const middlewares = jsonServer.defaults({ noCors: true });
+const middlewares = jsonServer.defaults();
 const router = jsonServer.router("db.json");
 const port = process.env.PORT || 25545;
+const cors = require("cors");
+
+server.use(
+  cors({
+    origin: true,
+    credentials: true,
+    preflightContinue: false,
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  })
+);
+server.options("*", cors());
 
 server.use(middlewares);
 server.use(router);
