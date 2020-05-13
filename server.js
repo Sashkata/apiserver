57332fae86a3491f8eaa5c2df75b931273f266b2
Sashkata <alekspenev347@gmail.com>
2020-05-13 11:08:04 -0500

    Changing cors policy

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const jsonServer = require("json-server");
 const server = jsonServer.create();
+const middlewares = jsonServer.defaults({ noCors: true });
 const router = jsonServer.router("db.json");
 const port = process.env.PORT || 25545;
 
+server.use(middlewares);
 server.use(router);
 server.listen(port, () => {
   console.log("JSON SERVER IS RUNNING");
+  console.log({ port });
 });
